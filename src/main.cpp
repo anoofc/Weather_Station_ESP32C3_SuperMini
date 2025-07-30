ba0e842f64e543e615734a9b71f2d9374d3f93ce
anoofc <anoof7262@gmail.com>
2025-07-30 15:34:06 +0400

    Update display handling and sensor reading intervals; refactor display update functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#define DEBUG 		1                         // 0 or 1 to turn OFF or ON Debugging
+#define DEBUG 		0                         // 0 or 1 to turn OFF or ON Debugging
 
 #include <Arduino.h>
 #include <Wire.h>
@@ -6,12 +6,13 @@
 #include <Adafruit_BMP280.h>
 #include <AHTxx.h>
 #include <ScioSense_ENS160.h>
-// #include <u8g2lib.h>
 
 #include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h> // Include Adafruit GFX library for graphics support
+#include <Fonts/FreeSerifBold9pt7b.h>
+#include <Fonts/FreeSerifBold12pt7b.h>
 
-
-#define SEALEVELPRESSURE_HPA (1013.25)      // Sea Level Atmospheric Pressure in hPA
+#define SEALEVELPRESSURE_HPA (1000.0)      // Sea Level Atmospheric Pressure in hPA
 
 #define SoftI2C_SDA   8  
 #define SoftI2C_SCL   7  
@@ -20,6 +21,10 @@
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
+
+
+#define DISPLAY_UPDATE_INTERVAL 5000 // Update display every 1000 ms (1 second)
+#define SENSOR_READ_INTERVAL    100  // Read sensors every 1000 ms (1 second)
 
 TwoWire Wire_Main = TwoWire(0);         // I2C bus 0
 TwoWire Wire_BMP 	= TwoWire(1);         // I2C bus 1 
@@ -30,15 +35,14 @@ ScioSense_ENS160 ens160(&Wire_Main, ENS160_I2CADDR_1);    // Create ENS160 objec
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire_Main, -1);
 
-
-// U8G2_SSD1306_128X64_NONAME_1_SW_I2C display(U8G2_R0, /* clock=*/ HardI2C_SCL, /* data=*/ HardI2C_SDA, /* reset=*/ U8X8_PIN_NONE);
-// U8G2_SSD1306_128X64_NONAME_1_HW_I2C display(U8G2_R0, U8X8_PIN_NONE);
-
 // VARIABLES TO HOLD SENSOR DATA
 float pressure, altitude, temperature, humidity;
 uint8_t aqi;
 uint16_t tvoc, eco2, aqi500;
 
+uint8_t  showSensor = 0;          // Variable to control which sensor data to show on the display
+uint32_t displayUpdateTimer = 0;  // Variable to store the last time the display was updated
+uint32_t sensorReadTimer = 0;     // Variable to store the last time the sensors were read
 
 void readBMP280(){
 	pressure = bmp.readPressure() / 100.0F;
@@ -64,24 +68,47 @@ void readENS160(){
   }
 }
 
-// void updateDisplay(){
-//   display.clearBuffer();					// clear the internal memory
-//   display.setFont(u8g2_font_ncenB12_tr);	// choose a suitable font
-//   display.drawStr(0,30,"Hello World!");	// write something to the internal memory
-//   display.sendBuffer();					// transfer internal memory to the display
-// }
-
-void updateDisplay() {
+void updateDisplay_bmp() {
   display.clearDisplay();
 
   display.setTextSize(1);      // Normal 2:1 pixel scale
-  display.setTextColor(SSD1306_WHITE); // Draw white text
-  display.setCursor(0, 30);    // Start at top-left corner, y=20 for vertical centering
-  display.println(F("Hello World!"));
 
+  display.setCursor(10, 25);    // Start at top-left corner, y=20 for vertical centering
+  display.setFont(&FreeSerifBold12pt7b); 
+  display.print(pressure, 2);  display.setFont(&FreeSerifBold9pt7b); display.println(F(" hPa"));
+  display.setCursor(10, 55);    // Move cursor down for next line
+  display.setFont(&FreeSerifBold12pt7b); 
+  display.print(altitude, 2);  display.setFont(&FreeSerifBold9pt7b); display.println(F(" m"));
   display.display();           // Show the text on the display
 }
 
+void updateDisplay_aht21() {
+  display.clearDisplay();
+  display.setTextSize(1);      // Normal 2:1 pixel scale
+
+  display.setCursor(10, 25);    // Start at top-left corner, y=20 for vertical centering
+  display.setFont(&FreeSerifBold12pt7b);
+  display.print(temperature, 2);   display.setFont(&FreeSerifBold9pt7b); display.println(F(" °C"));
+  display.setCursor(10, 55);    // Move cursor down for next line
+  display.setFont(&FreeSerifBold12pt7b); 
+  display.print(humidity, 2);   display.setFont(&FreeSerifBold9pt7b); display.println(F(" %"));
+  display.display();           // Show the text on the display
+}
+
+void updateDisplay_ens160() {
+  display.clearDisplay();
+  display.setTextSize(1);      // Normal 2:1 pixel scale
+
+  display.setCursor(5, 15);    // Start at top-left corner, y=20 for vertical centering
+  display.setFont(&FreeSerifBold9pt7b); 
+  display.print(F("AQI : ")); display.setFont(&FreeSerifBold12pt7b); display.println(aqi);          
+  display.setCursor(5, 35);    // Move cursor down for next line
+  display.setFont(&FreeSerifBold9pt7b); 
+  display.println(F("eCO2:"));  
+  display.setCursor(30, 60);    // Move cursor down for next line
+  display.setFont(&FreeSerifBold12pt7b); display.print(eco2); display.setFont(&FreeSerifBold9pt7b); display.println(F(" ppm"));
+  display.display();           // Show the text on the display
+}
 
 void setup() {
 	if (DEBUG) { Serial.begin(115200); }     // Initialize Serial Communication for Debugging
@@ -91,8 +118,9 @@ void setup() {
 	// Initialize Custom I2C communication for BMP280
   Wire_BMP.begin(SoftI2C_SDA, SoftI2C_SCL);  delay(100); // Initialize I2C with custom pins
 
-	// display.begin(); // Initialize U8G2 with the main I2C bus
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  display.setTextColor(SSD1306_WHITE); // Draw white text
 
   // INITIALIZE SENSORS
   aht21.begin();                             // Initialize AHT21 sensor with custom I2C bus
@@ -102,31 +130,21 @@ void setup() {
 }
 
 void loop() {
-	readBMP280(); 		// Read Pressure sensor data
-	readaht21();	 		// Read AHT21 sensor data
-  readENS160();     // Read ENS160 Sensor data
-  if (DEBUG){Serial.println("---------------------------------------------------");}   // to seperate bw new & last sensor data 
-  delay(1000); 
-  updateDisplay();  // Update the OLED display with the latest data
-	delay(1000); 			// Wait for 1 second before the next reading
+  if (millis() - sensorReadTimer >= SENSOR_READ_INTERVAL) {
+    sensorReadTimer = millis(); // Reset the timer
+    readBMP280(); 		// Read Pressure sensor data
+    readaht21();	 		// Read AHT21 sensor data
+    readENS160();     // Read ENS160 Sensor data
+    if (DEBUG){Serial.println("---------------------------------------------------");}   // to seperate bw new & last sensor data 
 
+    if      (showSensor == 0) { updateDisplay_bmp(); } // Show BMP280 data
+    else if (showSensor == 1) { updateDisplay_aht21(); } // Show AHT21 data
+    else if (showSensor == 2) { updateDisplay_ens160(); } // Show ENS160 data
+  }
+  
+  if (millis() - displayUpdateTimer >= DISPLAY_UPDATE_INTERVAL) {
+    displayUpdateTimer = millis(); // Reset the timer
+    showSensor++; // Increment the sensor index
+    if (showSensor > 2) { showSensor = 0; } // Reset to 0 if it exceeds the number of sensors
+  }
 }
-
-// #include <Arduino.h>
-// #include <Wire.h>
-// #include <U8g2lib.h>
-
-
-// U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
-
-// void setup(void) {
-//   u8g2.begin();
-// }
-
-// void loop(void) {
-//   u8g2.clearBuffer();					// clear the internal memory
-//   u8g2.setFont(u8g2_font_ncenB12_tr);	// choose a suitable font
-//   u8g2.drawStr(0,30,"Hello World!");	// write something to the internal memory
-//   u8g2.sendBuffer();					// transfer internal memory to the display
-//   delay(1000);  
-// }
